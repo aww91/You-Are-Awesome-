0c96ccef61f391480038728407036a756c37539e
Anthony Whitfield <aww91@yahoo.com>
2022-06-23 12:47:04 -0400

    Code before While loop

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -12,10 +12,11 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     
-    var messages = ["You Are Awesome!!", "You Are Great!", "You Are Da Bomb!", "You Are Fantastic!"]
+    var messages = ["You Are Awesome!!", "You Are Great!", "You Are Da Bomb!", "You Are Fantastic!", "You've Got The Design Skills of Jony Ive"]
     var messageCount = 0
     
     var imageCount = 1
+    let totalNumberOfImages = 9
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,24 +24,29 @@ class ViewController: UIViewController {
     }
     
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        let awesomeMessage = "You are Awesome!"
-        let greatMessage = "You are Great!"
-        let bombMessage = "You Are Da Bomb"
         
-        if myMessage.text == awesomeMessage{
-            myMessage.text = greatMessage
-            imageView.image = UIImage(named: "image1")
-            
-        } else if myMessage.text == greatMessage {
-            myMessage.text = bombMessage
-            imageView.image = UIImage(named: "image2")
-            
-        }
-            else {
-                myMessage.text = awesomeMessage
-                imageView.image = UIImage(named: "image0")
-            }
-    }
+        myMessage.text = messages[Int.random(in: 0...(messages.count - 1))]
+        imageView.image = UIImage(named: "image" + String(Int.random(in: 0...totalNumberOfImages)))
+        
+    
+//        let awesomeMessage = "You are Awesome!"
+//        let greatMessage = "You are Great!"
+//        let bombMessage = "You Are Da Bomb"
+//
+//        if myMessage.text == awesomeMessage{
+//            myMessage.text = greatMessage
+//            imageView.image = UIImage(named: "image1")
+//
+//        } else if myMessage.text == greatMessage {
+//            myMessage.text = bombMessage
+//            imageView.image = UIImage(named: "image2")
+//
+//        }
+//            else {
+//                myMessage.text = awesomeMessage
+//                imageView.image = UIImage(named: "image0")
+//            }
+   }
     
 }
 
