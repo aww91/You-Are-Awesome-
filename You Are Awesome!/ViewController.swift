2b55089658103578cb61aaa48fc49a0d3f55db51
Anthony Whitfield <aww91@yahoo.com>
2022-06-28 09:48:37 -0400

    Saved 6/28/22

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -12,11 +12,17 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     
-    var messages = ["You Are Awesome!!", "You Are Great!", "You Are Da Bomb!", "You Are Fantastic!", "You've Got The Design Skills of Jony Ive"]
     var messageCount = 0
-    
     var imageCount = 1
     let totalNumberOfImages = 9
+    
+    var messages = ["You Are Awesome!!",
+                    "You Are Great!",
+                    "You Are Fantastic!",
+                    "When the Genius Bar Needs Help, They Call You!",
+                    "Fabulous? That's You!",
+                    "You've Got The Design Skills of Jony Ive"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,27 +31,27 @@ class ViewController: UIViewController {
     
     @IBAction func messageButtonPressed(_ sender: UIButton) {
         
-        myMessage.text = messages[Int.random(in: 0...(messages.count - 1))]
-        imageView.image = UIImage(named: "image" + String(Int.random(in: 0...totalNumberOfImages)))
+        var newMessage = messages[Int.random(in: 0...(messages.count - 1))]
+        while myMessage.text == newMessage {
+            print("*** We had a repeating value. Both newMessage and myMessage are the same \(newMessage) and \(myMessage.text)")
+            newMessage = messages[Int.random(in: 0...(messages.count - 1))]
+        }
+        myMessage.text = newMessage
+            
+        
+        
+        
+        
+        var newImageView = UIImage(named: "image" + String(Int.random(in: 0...totalNumberOfImages)))
+        
+        while imageView.image == newImageView {
+            print("*** We had a repeating value. Both newImageView and imageView are the same \(newImageView) and \(imageView.image)")
+            newImageView = UIImage(named: "image" + String(Int.random(in: 0...totalNumberOfImages)))
+        }
+        imageView.image = newImageView
         
     
-//        let awesomeMessage = "You are Awesome!"
-//        let greatMessage = "You are Great!"
-//        let bombMessage = "You Are Da Bomb"
-//
-//        if myMessage.text == awesomeMessage{
-//            myMessage.text = greatMessage
-//            imageView.image = UIImage(named: "image1")
-//
-//        } else if myMessage.text == greatMessage {
-//            myMessage.text = bombMessage
-//            imageView.image = UIImage(named: "image2")
-//
-//        }
-//            else {
-//                myMessage.text = awesomeMessage
-//                imageView.image = UIImage(named: "image0")
-//            }
+
    }
     
 }
